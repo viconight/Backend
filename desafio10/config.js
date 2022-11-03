b25edf429442d685ff109573a3417aa9572c680c
viconight <viconight89@gmail.com>
2022-11-02 23:05:30 -0300

    desafio 10

---
+++ desafio10/config.js
@@ -0,0 +1,11 @@
+export default {
+    fileSystem: {
+        path: './db'
+    },
+    mongoDB: {
+        URI: 'mongodb://localhost:27017/desafio10'
+      },
+    firebase: {
+        serviceAccount: '../backend-bdb66-firebase-adminsdk-d3162-a0196eb6a61.json'
+    },
+  }
---
+++ desafio10/routers/productos.js
@@ -0,0 +1,81 @@
+import { Router } from 'express'
+import { productosDao as productosApi } from '../daos/index.js'
+
+const router = Router();
+
+const administrador = true;
+
+router.get('/', async (req, res) => {
+    
+    const productos = await productosApi.listarAll();
+    res.status(200).json(productos);
+})
+
+router.get('/:id', async (req, res) => {
+    
+    const producto = await productosApi.listar(req.params.id);
+    if (producto) {
+        res.status(200).json(producto);
+    } else {
+        res.status(404).json({
+            error: -2,
+            descripcion: `ruta http://localhost:8080/api/productos/${req.params.id} metodo GET no implementada` // 404 NOT FOUND
+         })
+    }
+})
+
+router.post('/', async (req, res) => {
+   
+    const productos = await productosApi.guardar(req);
+    if (administrador) {
+        // res.status(201).json(productos);
+        res.redirect('/');
+    } else {
+        res.status(403).json({
+            error: -1,
+            descripcion: 'ruta http://localhost:8080/api/productos/ metodo POST no autorizada ' // 403 FORBIDDEN
+        })
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    
+    const producto = await productosApi.actualizar(req.params.id, req);
+    if (administrador) {
+        if (producto) { 
+            res.status(204).end();
+        } else (
+            res.status(404).json({
+                error: -2,
+                descripcion: `ruta http://localhost:8080/api/productos/${req.params.id} metodo PUT no implementada` // 404 NOT FOUND
+             })
+        )
+    } else {
+        res.status(403).json({
+            error: -1,
+            descripcion: 'ruta http://localhost:8080/api/productos/ metodo PUT no autorizada '
+        })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    
+    const producto = await productosApi.borrar(req.params.id);
+    if (administrador) {
+        if (producto) {
+            res.status(204).end();
+        } else (
+            res.status(404).json({
+                error: -2,
+                descripcion: `ruta http://localhost:8080/api/productos/${req.params.id} metodo DELETE no implementada` // 404 NOT FOUND
+             })
+        )
+    } else {
+        res.status(403).json({
+            error: -1,
+            descripcion: 'ruta http://localhost:8080/api/productos/ metodo DELETE no autorizada ' // 403 FORBIDDEN
+        })
+    }
+})
+
+export default router
